8ee79721a20c0bfdf63ecad5d0557cb5acfd8770
Srushti <srushti.singh@epfl.ch>
2024-05-05 16:23:11 +0200

    committing changes

--- srushti/script.py
+++ srushti/script.py
@@ -14,9 +14,8 @@ for document in dataset:
     if all_utterance:
         first_utterance = all_utterance[0]
         inputs = tokenizer(first_utterance, return_tensors="pt")
-        outputs = model.generate(**inputs, max_length=50)     #max_length is the max_length of the response of the model,
-                                                            # temperature is the randomness...ask how to set these parameters
+        outputs = model.generate(**inputs, max_length=50)  # max_length is the max_length of the response of the model,
+        # temperature is the randomness...ask how to set these parameters
         text = tokenizer.decode(outputs[0])
         print("Input:", first_utterance)
         print("Generated Response:", text)
-
