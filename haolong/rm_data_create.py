a0097ef155c15f7451d9a2f6f58b28e155fb57bb
Tachi-67 <haolongli_neko@outlook.com>
2024-06-04 01:27:12 +0800

    add batch_size for pipeline

--- haolong/rm_data_create.py
+++ haolong/rm_data_create.py
@@ -314,8 +314,8 @@ if __name__ == "__main__":
     model_sl.generation_config.pad_token_id = model_sl.generation_config.eos_token_id
     model_cons.generation_config.pad_token_id = model_cons.generation_config.eos_token_id
 
-    pipe_SL = pipeline("text-generation", model=model_sl, device_map="auto", tokenizer=tokenizer)
-    pipe_constitution = pipeline("text-generation", model=model_cons, device_map="auto", tokenizer=tokenizer)
+    pipe_SL = pipeline("text-generation", model=model_sl, device_map="auto", tokenizer=tokenizer, batch_size=32)
+    pipe_constitution = pipeline("text-generation", model=model_cons, device_map="auto", tokenizer=tokenizer, batch_size=32)
     
     print("Pipelines loaded")
     print("SL device:", pipe_SL.device)
