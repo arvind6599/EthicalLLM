b0c52eb09cc6fdc63d1ade08dafa7b69deb0a521
Tachi-67 <haolongli_neko@outlook.com>
2024-06-04 02:02:01 +0800

    up sample size

--- haolong/rm_data_create.py
+++ haolong/rm_data_create.py
@@ -226,7 +226,7 @@ if __name__ == "__main__":
                 ]
     
     # sampling for small scale testing
-    harmful_prompts = random.choices(harmful_prompts, k=130)
+    harmful_prompts = random.choices(harmful_prompts, k=256)
     
     action_data = pd.DataFrame(columns=['conversation', 'scores'])
     motivation_data = pd.DataFrame(columns=['conversation', 'scores'])
