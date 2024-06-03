33469c16b650d11785a01cfaff96fe3cb143a6ff
Tachi-67 <haolongli_neko@outlook.com>
2024-06-03 23:04:57 +0800

    add device test

--- haolong/rm_data_create.py
+++ haolong/rm_data_create.py
@@ -171,10 +171,10 @@ if __name__ == "__main__":
     # )
 
 
-    pipe_SL = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.2", device_map=device,
+    pipe_SL = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.2", device_map="auto",
                     # quantization_config=quantization_config, # remove this after
                     )
-    pipe_constitution = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.2", device_map=device,
+    pipe_constitution = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.2", device_map="auto",
                                 # quantization_config=quantization_config, # remove this after
                                 )
     
