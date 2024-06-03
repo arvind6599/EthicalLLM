3dfa568b31bcbf2e00c2d3f75d49c5669d787a66
Tachi-67 <haolongli_neko@outlook.com>
2024-06-04 01:37:40 +0800

    adjust params

--- haolong/rm_data_create.py
+++ haolong/rm_data_create.py
@@ -10,85 +10,6 @@ import random
 import time
 from utils import retrieve_and_parse_response, load_harmful_data, reconstruct_conversation, add_rows_to_df
 
-
-# def retrieve_pipe_response(pipe, prompt, max_new_tokens=32, type_pipe="SFT"):
-#     """
-#     Given a prompt, return the response from the pipeline
-#     :param pipe: the pipeline to use
-#     :param prompt: the prompt to use to the LM
-#     :param max_new_tokens: the maximum number of tokens to generate
-#     :return 
-#         - if type_pipe == "SFT", return_dict = {"action": action, "motivation": motivation, "consequence": consequence}
-#         - if type_pipe == "Constitution", returns the JSON parsable data from the model response
-#     """
-#     # If type is SFT, for the harmful prompt input, we ask action & motivation & consequence
-#     # We ask the 3 questions sequentially by appending the chat history & user query
-#     if type_pipe == "SFT":
-#         return_dict = {}
-#         # retriving action
-#         messages = [
-#             {
-#                 "role":"user",
-#                 "content":prompt
-#             }
-#         ]
-#         answer_from_agent = pipe(messages, max_new_tokens=max_new_tokens, do_sample=True)[0]['generated_text'][-1]
-#         return_dict["action"] = answer_from_agent['content']
-        
-#         # retriving motivation
-#         messages.append(answer_from_agent)
-#         messages.append({
-#             "role":"user",
-#             "content":"What is the motivation behind the answer?"
-#         })
-#         motivation_from_agent = pipe(messages, max_new_tokens=max_new_tokens, do_sample=True)[0]['generated_text'][-1]
-#         return_dict["motivation"] = motivation_from_agent['content']
-        
-#         # retriving concequence
-#         messages.append(motivation_from_agent)
-#         messages.append({
-#             "role":"user",
-#             "content":"What is the utility of the action towards promoting your virtuous character?"
-#         })
-#         consequence_from_agent = pipe(messages, max_new_tokens=max_new_tokens, do_sample=True)[0]['generated_text'][-1]
-#         return_dict["consequence"] = consequence_from_agent['content']
-#         return return_dict
-    
-#     # If type is constitution, we ask the constitution model of the prompts generated from generate_prompt_to_constitution (see below)
-#     # Most of the work here relates to paring the output from the model (see below, also see utils.py/retrieve_and_parse_response)
-#     elif type_pipe == "Constitution":
-#         return_dict = {}
-#         messages = [
-#             {
-#                 "role":"user",
-#                 "content":prompt
-#             }
-#         ]
-#         # If output is cannot be handled by the parser, we ask the LM again for a valid response
-#         # We don't keep asking in case the model simply fails to do so and stop.
-#         # TODO: for verbosity, maybe consider skipping the current instance if things dont work out now.
-#         cnt = 0
-#         while True:
-#             answer_from_agent = pipe(messages, max_new_tokens=max_new_tokens, do_sample=True)[0]['generated_text'][-1]
-#             answer_content = answer_from_agent['content']
-#             cnt += 1
-#             if cnt > 3:
-#                 print("********")
-#                 print(answer_content)
-#                 print("********")
-#                 raise IndexError("Too many iterations when trying to parse")
-#             try:
-#                 return_dict = retrieve_and_parse_response(answer_content)
-#                 return return_dict
-#             except ValueError:
-#                 messages.append(answer_from_agent)
-#                 messages.append({
-#                     "role":"user",
-#                     "content":f"Previous respond is not in JSON format, please respond in JSON format within {max_new_tokens} tokens"
-#                 })
-#                 continue
-
-# batch processing version of the previous function
 def retrieve_pipe_responses(pipe, prompts, max_new_tokens=32, type_pipe="SFT"):
     """
     Given a list of prompts, return the responses from the pipeline in batch
@@ -194,34 +115,6 @@ def generate_prompt_to_constitution(virtue: str, harmful_prompt_to_SFT_model: st
     """
     return prompt_to_constitution
 
-# def prepare_new_row_for_amc_data(harmful_prompt:str, response_1: str, response_2: str, virtues, type='action'):
-#     assert type in ['action', 'motivation', 'consequence'], "type must be one of action, motivation, consequence"
-#     new_row_1 = pd.Series({'conversation': reconstruct_conversation(harmful_prompt, response_1), 'scores': 0})
-#     new_row_2 = pd.Series({'conversation': reconstruct_conversation(harmful_prompt, response_2), 'scores': 0})
-#     for virtue in virtues:
-#         prompt_to_constitution = generate_prompt_to_constitution(virtue, harmful_prompt, [response_1, response_2], type=type)
-#         constitution_model_response = retrieve_pipe_response(pipe_constitution, prompt_to_constitution, max_new_tokens=128, type_pipe="Constitution")
-        
-#         if 'choice' in constitution_model_response:
-#             if constitution_model_response['choice'] == 'A':
-#                 new_row_1['scores'] += 1
-#             elif constitution_model_response['choice'] == 'B':
-#                 new_row_2['scores'] += 1
-#         else:
-#             response_dict_values = list(constitution_model_response.values())
-#             if 'A' in response_dict_values or 'a' in response_dict_values:
-#                 new_row_1['scores'] += 1
-#             elif 'B' in response_dict_values or 'b' in response_dict_values:
-#                 new_row_2['scores'] += 1
-#             else:
-#                 print(constitution_model_response)
-#                 raise KeyError("wtf??")
-            
-#     new_row_1['scores'] = new_row_1['scores'] / len(virtues)
-#     new_row_2['scores'] = new_row_2['scores'] / len(virtues)
-#     return new_row_1, new_row_2
-
-# batch processing version of the previous function
 def prepare_new_rows_for_amc_data(harmful_prompts, responses_1, responses_2, virtues, type='action'):
     new_rows_1 = []
     new_rows_2 = []
@@ -239,7 +132,7 @@ def prepare_new_rows_for_amc_data(harmful_prompts, responses_1, responses_2, vir
             prompt_to_index_virtue_map.append((index, virtue))
     
     # process all prompts in batch
-    constitution_responses = retrieve_pipe_responses(pipe_constitution, all_prompts, max_new_tokens=20, type_pipe="Constitution")
+    constitution_responses = retrieve_pipe_responses(pipe_constitution, all_prompts, max_new_tokens=16, type_pipe="Constitution")
 
     # initialize scores
     scores_1 = [0] * len(harmful_prompts)
@@ -310,8 +203,8 @@ if __name__ == "__main__":
     model_sl.generation_config.pad_token_id = model_sl.generation_config.eos_token_id
     model_cons.generation_config.pad_token_id = model_cons.generation_config.eos_token_id
 
-    pipe_SL = pipeline("text-generation", model=model_sl, device_map="auto", tokenizer=tokenizer, batch_size=32)
-    pipe_constitution = pipeline("text-generation", model=model_cons, device_map="auto", tokenizer=tokenizer, batch_size=32)
+    pipe_SL = pipeline("text-generation", model=model_sl, device_map="auto", tokenizer=tokenizer, batch_size=64)
+    pipe_constitution = pipeline("text-generation", model=model_cons, device_map="auto", tokenizer=tokenizer, batch_size=64)
     
     pipe_SL.tokenizer.pad_token_id = model_sl.config.eos_token_id
     pipe_constitution.tokenizer.pad_token_id = model_cons.config.eos_token_id
@@ -339,44 +232,19 @@ if __name__ == "__main__":
     motivation_data = pd.DataFrame(columns=['conversation', 'scores'])
     consequences_data = pd.DataFrame(columns=['conversation', 'scores'])
     
-
-    # NOTE: DEPRECATED, slow
-    # for harmful_prompt in tqdm(harmful_prompts, desc="Processing prompts"):
-    #     SFT_model_response_dict_1 = retrieve_pipe_responses(pipe_SL, harmful_prompt, max_new_tokens=32)
-    #     SFT_model_response_dict_2 = retrieve_pipe_responses(pipe_SL, harmful_prompt, max_new_tokens=32)
-        
-    #     new_action_data_row_1, new_action_data_row_2 = prepare_new_row_for_amc_data(harmful_prompt,
-    #                                                                                 SFT_model_response_dict_1["action"],
-    #                                                                                 SFT_model_response_dict_2["action"],
-    #                                                                                 type='action',
-    #                                                                                 virtues=virtues)
-    #     new_motivation_data_row_1, new_motivation_data_row_2 = prepare_new_row_for_amc_data(harmful_prompt,
-    #                                                                                         SFT_model_response_dict_1["motivation"],
-    #                                                                                         SFT_model_response_dict_2["motivation"],
-    #                                                                                         type='motivation',
-    #                                                                                         virtues=virtues)
-    #     new_consequence_data_row_1, new_consequence_data_row_2 = prepare_new_row_for_amc_data(harmful_prompt,
-    #                                                                                         SFT_model_response_dict_1["consequence"],
-    #                                                                                         SFT_model_response_dict_2["consequence"],
-    #                                                                                         type='consequence',
-    #                                                                                         virtues=virtues)
-        
-    #     add_rows_to_df(action_data, [new_action_data_row_1, new_action_data_row_2])
-    #     add_rows_to_df(motivation_data, [new_motivation_data_row_1, new_motivation_data_row_2])
-    #     add_rows_to_df(consequences_data, [new_consequence_data_row_1, new_consequence_data_row_2])
     
     cur_time = time.time()
     start_time = cur_time
     
     # ask twice
-    sft_responses_1 = retrieve_pipe_responses(pipe_SL, harmful_prompts, max_new_tokens=32, type_pipe="SFT")
+    sft_responses_1 = retrieve_pipe_responses(pipe_SL, harmful_prompts, max_new_tokens=128, type_pipe="SFT")
     
     ########################################
     print(f"Time elapsed for SFT 1: {time.time() - cur_time}")
     cur_time = time.time()
     ########################################
     
-    sft_responses_2 = retrieve_pipe_responses(pipe_SL, harmful_prompts, max_new_tokens=32, type_pipe="SFT")
+    sft_responses_2 = retrieve_pipe_responses(pipe_SL, harmful_prompts, max_new_tokens=128, type_pipe="SFT")
     
     ########################################
     print(f"Time elapsed for SFT 2: {time.time() - cur_time}")
