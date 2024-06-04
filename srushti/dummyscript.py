f82f9e6f3169d5993ccdee3f50159d74a8a35ffd
Srushti <srushti.singh@epfl.ch>
2024-06-05 01:37:22 +0200

    testing if anything changes

--- srushti/dummyscript.py
+++ srushti/dummyscript.py
@@ -15,9 +15,9 @@ tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", 
 
 tokenizer.pad_token_id = tokenizer.eos_token_id
 
-batch_size = 15
+batch_size = 16
 revised_data = []
-dataset = load_dataset("DIBT/10k_prompts_ranked")
+dataset = load_dataset("fka/awesome-chatgpt-prompts")
 num_prompts = len(dataset)
 
 # Access the 'train' split
@@ -26,7 +26,7 @@ train_dataset = dataset['train']
 # Extract prompts from the 'train' split
 dataset_prompt = train_dataset['prompt']
 model.to(device)
-
+dataset_prompt = dataset_prompt[:32]
 
 class RevisionModel(nn.Module):
     def __init__(self, model, tokenizer, principles_list):
@@ -48,8 +48,9 @@ class RevisionModel(nn.Module):
                             f" Remove the count of tokens in the revised answer such as '(50 tokens)'."
                             f" Remove any '\n\n Revised response: \n\n' from the revised response."
                             )
-                new_inputs = self.tokenizer.encode(critique, return_tensors='pt').to(device)
-                new_out = self.model.generate(new_inputs, max_new_tokens=50)
+                new_inputs = self.tokenizer.encode(critique, return_tensors='pt').to(self.model.device)
+                with torch.no_grad():
+                    new_out = self.model.generate(new_inputs, max_new_tokens=50)
                 new_out = new_out[:, len(new_inputs[0]):]
                 criticized_answer = self.tokenizer.decode(new_out[0], skip_special_tokens=True)
                 base_answer = criticized_answer
@@ -59,18 +60,22 @@ class RevisionModel(nn.Module):
 
 def revision(principles_list):
     revised_data = []
+
+    revision_model = RevisionModel(model, tokenizer, principles_list).to(device)
+    revision_model = nn.DataParallel(revision_model)
+
     for i in tqdm(range(0, len(dataset_prompt), batch_size)):
         batch_prompts = dataset_prompt[i:i + batch_size]
         input_ids = tokenizer.batch_encode_plus(batch_prompts, return_tensors='pt', padding=True)
         input_ids = input_ids.input_ids.to(device)
-
-        output = model.generate(input_ids, max_new_tokens=50)
+        with torch.no_grad():
+            output = model.generate(input_ids, max_new_tokens=50)
         output = output[:, input_ids.shape[1]:]
 
         base_answers = [tokenizer.decode(out, skip_special_tokens=True) for out in output]
 
-        revision_model = RevisionModel(model, tokenizer, principles_list).to(device)
-        revision_model = nn.DataParallel(revision_model)
+        #revision_model = RevisionModel(model, tokenizer, principles_list).to(device)
+        #revision_model = nn.DataParallel(revision_model)
 
         revised_answers = revision_model(batch_prompts, base_answers)
 
@@ -81,7 +86,9 @@ def revision(principles_list):
                 "revised_answer": revised_answers[j]
             })
 
-    with open("revised_datafile_10k.jsonl", "w") as f:
+        torch.cuda.empty_cache()
+
+    with open("revised_datafile.jsonl", "w") as f:
         for data in revised_data:
             json_line = json.dumps(data)
             f.write(json_line + "\n")
