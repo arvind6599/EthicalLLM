7fe179c55dc0e5b6a88386ff02d33eb8e20b73f5
Tachi-67 <haolongli_neko@outlook.com>
2024-06-05 19:30:35 +0800

    revert to previous adaptor merger

--- haolong/adaptor_merger.py
+++ haolong/adaptor_merger.py
@@ -2,7 +2,7 @@ from peft import AutoPeftModelForSequenceClassification
 import torch
 from transformers import AutoTokenizer
 
-def merge_and_upload(type = 'motivation', tokenizer = None):
+def merge_and_upload(type = 'action', tokenizer = None):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model_id = 'Tachi67/EthcalLLM-RM-' + type
     peft_model = AutoPeftModelForSequenceClassification.from_pretrained(model_id)
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2")
     if tokenizer.pad_token is None:
             tokenizer.add_special_tokens({'pad_token': tokenizer.eos_token})
-    for type in ['motivation', 'consequences']:
+    for type in ['action','motivation', 'consequences']:
         merge_and_upload(type, tokenizer)
     print("All models uploaded!")
