536b68f1f592b2b77a0998da429a07e5c42445b7
Srushti <srushti.singh@epfl.ch>
2024-06-07 13:49:47 +0200

    script sft

--- srushti/script.py
+++ srushti/script.py
@@ -12,21 +12,34 @@ import json
 import random
 from sklearn.model_selection import train_test_split
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # the device to load the model onto
-
 access_token = "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"
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", use_cache=False, token=access_token)
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", token=access_token)
-model.to(device)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")  # the device to load the model onto
+device_map = {"": int(os.environ.get("LOCAL_RANK") or 0)}
+quantization_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16,
+)
+config = AutoConfig.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", num_labels=2, token=access_token)
+model_for_classification = AutoModelForSequenceClassification.from_pretrained(
+    "mistralai/Mistral-7B-Instruct-v0.2",
+    config=config,
+    quantization_config=quantization_config,
+    device_map=device_map
+)
 
-tokenizer.pad_token = tokenizer.eos_token
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", model_max_length=100,
+                                          padding="max_length", truncation=True)
+if tokenizer.pad_token is None:
+    tokenizer.add_special_tokens({'pad_token': tokenizer.eos_token})
+    model_for_classification.resize_token_embeddings(len(tokenizer))
+model_for_classification.config.pad_token_id = model_for_classification.config.eos_token_id
 
 dataset = load_dataset("srushtisingh/Ethical")  # Replace "your_data.jsonl" with your file path
 
 
 def tokenize_function(examples):
-    return tokenizer(examples["prompt"], text_target=examples["revised_answer"], truncation=True, padding="max_length",
-                     max_length=60)
+    return tokenizer(examples["prompt"], text_target=examples["revised_answer"], truncation=True, padding="max_length")
 
 
 # Tokenize dataset
