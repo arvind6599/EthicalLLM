2f5aec87d648e7511baac255c4183894d2b6ef10
Srushti <srushti.singh@epfl.ch>
2024-06-07 12:35:14 +0200

    script sft

--- srushti/script.py
+++ srushti/script.py
@@ -1,8 +1,8 @@
 import torch
 # from transformers import GPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments, DataCollatorForLanguageModeling
-# from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, TrainingArguments, Trainer
+# from transformers import AutoModelForCausalLM, , BitsAndBytesConfig, TrainingArguments, Trainer
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, TextDataset, DataCollatorForLanguageModeling
-from transformers import Trainer, TrainingArguments, AutoModelForCausalLM
+from transformers import Trainer, TrainingArguments, AutoModelForCausalLM, AutoTokenizer
 import torch
 from datasets import load_dataset
 from datasets import Dataset
