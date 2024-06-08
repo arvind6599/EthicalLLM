9e7ea99217164498c31a5cb932eaf9cbd56e558f
Srushti <srushti.singh@epfl.ch>
2024-06-08 23:57:24 +0200

    script sft

--- srushti/script.py
+++ srushti/script.py
@@ -28,7 +28,7 @@ quantization_config = BitsAndBytesConfig(
     bnb_4bit_quant_type="nf4",
     bnb_4bit_compute_dtype=torch.float16,
 )
-config = AutoConfig.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", num_labels=2, use_auth_token=access_token)
+config = AutoConfig.from_pretrained("mistralai/Mistral-7B-Instruct-v0.2", num_labels=1, use_auth_token=access_token)
 
 # lora config
 LORA_R = 8
