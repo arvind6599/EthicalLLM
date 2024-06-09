574bfb9563689e3cee0d4b6bb20d53ab4c3c4063
Srushti <srushti.singh@epfl.ch>
2024-06-09 20:08:10 +0200

    testing

--- srushti/testSFT.py
+++ srushti/testSFT.py
@@ -1,6 +1,6 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
-
+from transformers import pipeline
 
 # Load the fine-tuned model and tokenizer from the Hugging Face Hub
 model_name = "srushtisingh/EthicalSFT"  # replace with your model name
@@ -10,7 +10,7 @@ model = AutoModelForCausalLM.from_pretrained(model_name)
 # Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
-
+'''
 # Define a test prompt
 test_prompts = [
     "How do I teach kids to meditate?",
@@ -18,9 +18,8 @@ test_prompts = [
     "Can you explain the importance of ethical considerations in technology?"
 ]
 
-
 # Function to generate responses
-def generate_response(prompt, model, tokenizer, max_length=250, num_return_sequences=1):
+def generate_response(prompt, model, tokenizer, max_length=100, num_return_sequences=1):
     inputs = tokenizer(prompt, return_tensors="pt").to(device)
     outputs = model.generate(
         inputs.input_ids,
@@ -40,3 +39,13 @@ for prompt in test_prompts:
     for i, response in enumerate(responses):
         print(f"Response {i + 1}: {response}")
     print("\n")
+'''
+# Use a pipeline as a high-level helper
+
+messages = [
+    {"role": "user", "content": "How do I teach kids to meditate?"},
+    {"role": "user", "content": "Can you explain the importance of ethical considerations in technology?"},
+    {"role": "user", "content": "Who are you?"},
+]
+pipe = pipeline("text-generation", model="srushtisingh/Ethicalsft")
+pipe(messages)
